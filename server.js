add820e112bd96f155d9dbc91bffce16ee394af0
dedoa82 <67242566+dedoa82@users.noreply.github.com>
2021-08-13 18:33:44 -0700

    CORS Error Handling

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const cors = require('cors')
+
 const path = require('path');
 
 const app = express();
@@ -13,6 +15,8 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+
+app.use(cors())
 
 app.use(express.static('public'))
 
